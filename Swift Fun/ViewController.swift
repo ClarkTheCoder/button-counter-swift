955d05bfd25c1012a0d28394803fb8f240af81a2
Carson <carsonclark2009@gmail.com>
2018-05-22 19:29:25 -0700

    added some comments

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -5,19 +5,43 @@
 //  Created by Carson Clark on 2018-05-22.
 //  Copyright © 2018 Carson Clark. All rights reserved.
 //
+// Visual representation of Main.storyboard
+// Lets GO..
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // starts at 0
+    var buttonCounter = 0
+    
+    // linking/referencing label element
+    @IBOutlet weak var myLabel: UILabel!
+    
+    // each time the button is tapped buttonCounter adds 1 to the current value
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        buttonCounter = buttonCounter + 1
+        
+        if buttonCounter > 10 {
+            myLabel.text = "You've hit the button over 10 times you nutter"
+        }
+        
+        if buttonCounter > 20 {
+            myLabel.text = "You've hit the button over 20 times you nutter"
+        }
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+	
+        myLabel.text = "Button Click Counter"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
